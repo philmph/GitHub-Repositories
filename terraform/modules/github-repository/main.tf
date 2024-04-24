9c00b3e0e9830206d06bf8aff9b2c274d51f52f4
Philipp Maier <git@pmaier.at>
2024-04-24 21:32:27 +0200

    Add default branch creation "main"

--- terraform/modules/github-repository/main.tf
+++ terraform/modules/github-repository/main.tf
@@ -4,6 +4,8 @@ resource "github_repository" "this" {
   visibility  = local.visibility
   archived    = local.archived
 
+  auto_init = true
+
   # Optional settings
   gitignore_template = local.options.gitignore_template
   has_issues         = local.options.has_issues
@@ -12,4 +14,14 @@ resource "github_repository" "this" {
   has_wiki           = local.options.has_wiki
   has_downloads      = local.options.has_downloads
   license_template   = local.options.license_template
+}
+
+resource "github_branch" "this" {
+  repository = github_repository.this.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "this" {
+  repository = github_repository.this.name
+  branch     = github_branch.this.branch
 }
