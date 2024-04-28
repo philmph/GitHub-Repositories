57c5e65c8bb1eb2b7c3b8f0c51470b2b5a947531
Philipp Maier <git@pmaier.at>
2024-04-28 10:01:20 +0200

    Update pins & remove static CLI config

--- terraform/env/terraform.tf
+++ terraform/env/terraform.tf
@@ -4,21 +4,12 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "= 6.2.1"
+      version = "6.2.1"
     }
 
     tfe = {
       source  = "hashicorp/tfe"
-      version = "= 0.53.0"
-    }
-  }
-
-  # TODO Research: Can this be dynamic?
-  cloud {
-    organization = "philmph"
-
-    workspaces {
-      name = "GitHub-Repositories"
+      version = "0.53.0"
     }
   }
 }
